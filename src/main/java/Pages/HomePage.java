1df0ac88a69f976afa4e46d608737aaa0103c5d3
Mazensayed07 <mazensayed89@gmail.com>
2026-09-17 22:32:57 +0300

    Add Home page and Quizzes Page

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -9,6 +9,8 @@ public class HomePage extends UtilityComponent {
     private final By quizzesBtn = By.cssSelector("a.nav-link[href='/StudentAnswer']");
     private final By myAttemptsNavLink = By.cssSelector("a.nav-link[href='/StudentAnswer/History']");
     private final By myAttemptsButtonLink = By.cssSelector("a.btn-outline-secondary[href='/StudentAnswer/History']");
+    private final By grade = By.cssSelector("p.page-sub strong");
+    private final By signOut = By.cssSelector("[type='submit']");
 
     public HomePage(WebDriver driver){
         super(driver);
@@ -17,6 +19,27 @@ public class HomePage extends UtilityComponent {
     public void clickStartQuiz(){
         click(startQuizBtn);
     }
+
+    public boolean isQuizPageDisplayed(){
+        return waitForTitleIs("Quizzes - Quiz");
+    }
+
+    public void clickMyAttempts(){
+        click(myAttemptsNavLink);
+    }
+
+    public boolean isMyAttemptsDisplayed() {
+        return waitForTitleIs("My attempts - Quiz");
+    }
+    public String getGrade(){
+        return getText(grade);
+    }
+    public void clickSignOut(){
+        click(signOut);
+    }
+
+
+
     
 
 
