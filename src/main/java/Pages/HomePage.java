0e501adb1df0c13dbc4234507f04f99c4132b72b
Mazensayed07 <mazensayed89@gmail.com>
2026-09-17 22:51:02 +0300

    Modification in Home Page Code

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -5,42 +5,69 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends UtilityComponent {
-    private final By startQuizBtn = By.cssSelector("a.btn-primary[href='/StudentAnswer']");
-    private final By quizzesBtn = By.cssSelector("a.nav-link[href='/StudentAnswer']");
-    private final By myAttemptsNavLink = By.cssSelector("a.nav-link[href='/StudentAnswer/History']");
-    private final By myAttemptsButtonLink = By.cssSelector("a.btn-outline-secondary[href='/StudentAnswer/History']");
-    private final By grade = By.cssSelector("p.page-sub strong");
-    private final By signOut = By.cssSelector("[type='submit']");
 
-    public HomePage(WebDriver driver){
+    // Locators
+    private final By startQuizBtn =
+            By.cssSelector("a.btn-primary[href='/StudentAnswer']");
+
+    private final By quizzesBtn =
+            By.cssSelector("a.nav-link[href='/StudentAnswer']");
+
+    private final By myAttemptsNavLink =
+            By.cssSelector("a.nav-link[href='/StudentAnswer/History']");
+
+    private final By myAttemptsButtonLink =
+            By.cssSelector("a.btn-outline-secondary[href='/StudentAnswer/History']");
+
+    private final By grade =
+            By.cssSelector("p.page-sub strong");
+
+    private final By signOut =
+            By.cssSelector("button.btn-outline-light");
+
+
+    // Constructor
+    public HomePage(WebDriver driver) {
         super(driver);
     }
 
-    public void clickStartQuiz(){
+
+    // Actions
+    public void clickStartQuiz() {
         click(startQuizBtn);
     }
 
-    public boolean isQuizPageDisplayed(){
-        return waitForTitleIs("Quizzes - Quiz");
+    public void clickQuizzes() {
+        click(quizzesBtn);
     }
 
-    public void clickMyAttempts(){
+    public void clickMyAttempts() {
         click(myAttemptsNavLink);
     }
 
-    public boolean isMyAttemptsDisplayed() {
-        return waitForTitleIs("My attempts - Quiz");
+    public void clickMyAttemptsButton() {
+        click(myAttemptsButtonLink);
     }
-    public String getGrade(){
-        return getText(grade);
-    }
-    public void clickSignOut(){
+
+    public void clickSignOut() {
         click(signOut);
     }
 
 
+    // Verifications
+    public boolean isHomePageDisplayed() {
+        return waitForTitleIs("Home - Quiz");
+    }
 
-    
+    public boolean isQuizzesPageDisplayed() {
+        return waitForTitleIs("Quizzes - Quiz");
+    }
 
+    public boolean isMyAttemptsPageDisplayed() {
+        return waitForTitleIs("My attempts - Quiz");
+    }
 
+    public String getGrade() {
+        return getText(grade);
+    }
 }
